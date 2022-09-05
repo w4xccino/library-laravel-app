96db9e9cebc8e878e793b098b88ec88b11e12353
Diego Toledo <dbtoledoa@gmail.com>
2022-09-05 12:02:22 -0500

    fix: typo register url

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-use Illuminate\Support\Fecades\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
