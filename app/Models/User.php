a2a88a6843d76bf8e1ed8f339f8441fc4908ca65
Diego Toledo <dbtoledoa@gmail.com>
2022-09-09 08:27:14 -0500

    feat(crypt): add hash as an encrypt method

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -43,6 +44,6 @@ class User extends Authenticatable
 
     public function setPasswordAttribute($value)
     {
-        $this->attributes["password"] = bcrypt($value);
+        $this->attributes["password"] = Hash::make($value);
     }
 }
