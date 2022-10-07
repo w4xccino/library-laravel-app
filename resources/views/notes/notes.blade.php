d6db9b182e4d55712881b620ab0dd4d95a402dd4
Diego Toledo <dbtoledoa@gmail.com>
2022-10-07 11:06:14 -0500

    feat: add timestamps to article

--- resources/views/notes/notes.blade.php
+++ resources/views/notes/notes.blade.php
@@ -5,11 +5,11 @@
     <a href="/add-note">
       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Add New Article</button>
     </a>
-    @foreach($articles as $article)
     <div class="flex mb-5 justify-center mt-2.5">
-      <div class="rounded-lg shadow-lg bg-white max-w-sm max-h-2 text-center">
+    @foreach($articles as $article)
+      <div class="text-center rounded-lg shadow-lg h-12">
         <div class="py-3 px-6 border-b border-gray-300">
-          Featured
+          {{$article->author->name}}
         </div>
         <div class="p-6">
           <h5 class="text-gray-900 text-xl font-medium mb-2">{{$article->title}}</h5>
@@ -18,16 +18,16 @@
           </p>
           <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View All</button>
         </div>
-        <div class="py-3 px-6 border-t border-gray-300 text-gray-600">
+        <div class="py-3 px-6 border-t border-gray-300 ">
           @foreach($article->images as $img)
-          <image height="10px" src="{{asset('/storage/images/').'/'.$img->name}}">
+          <image class="object-none" src="{{asset('/storage/images/').'/'.$img->name}}">
           @endforeach
         </div>
         <div class="py-3 px-6 border-t border-gray-300 text-gray-600">
-            {{$article->author->name}}
+            {{$article->created_at}}
         </div>
       </div>
-    </div>
     @endforeach
+    </div>
    </div>
 @endsection
